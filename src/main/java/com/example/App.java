699219b31fc0e84cad0e7ebe94b770ed4322cdd6
lai2-01 <lai2-01@itismeucci.com>
2023-10-09 09:50:30 +0200

    completazione client

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,10 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try{
+            Socket socket = new Socket("localhost",3000);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            out.writeBytes("ciao a tutti");
+            String risp = in.readLine();
+
+            System.out.println("Il server ha risposto: " + risp);
+
+            socket.close();
+            
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
